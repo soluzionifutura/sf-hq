5efd4d9d1aaa32db98715268d6c77e745f3f9d8c
Nicolo' Fuccella <nico@soluzionifutura.it>
2020-07-23 11:52:27 +0200

    Updates

---
+++ precache-manifest.a271069011d12f18abbb54d7896931e4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b44bdaae0549406d4c8857df74a28d",
+    "url": "/sf-hq/index.html"
+  },
+  {
+    "revision": "1aef9bab333309a1cd45",
+    "url": "/sf-hq/static/css/2.4fc996eb.chunk.css"
+  },
+  {
+    "revision": "9162b5943bcf79adfb23",
+    "url": "/sf-hq/static/css/main.7f73f10a.chunk.css"
+  },
+  {
+    "revision": "1aef9bab333309a1cd45",
+    "url": "/sf-hq/static/js/2.4fc5999f.chunk.js"
+  },
+  {
+    "revision": "22b32d1b33c6855d46e880d299959401",
+    "url": "/sf-hq/static/js/2.4fc5999f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9162b5943bcf79adfb23",
+    "url": "/sf-hq/static/js/main.ae0a55ce.chunk.js"
+  },
+  {
+    "revision": "46e3a96afa4bfb834b52",
+    "url": "/sf-hq/static/js/runtime-main.5539f58b.js"
+  },
+  {
+    "revision": "e69d12b5d93c32d18655d2d933d7788a",
+    "url": "/sf-hq/static/media/roboto-bold-webfont.e69d12b5.woff"
+  },
+  {
+    "revision": "ff82fb4145feceba44e1fd948170410e",
+    "url": "/sf-hq/static/media/roboto-bold-webfont.ff82fb41.woff2"
+  },
+  {
+    "revision": "35658d0ddef687620bfe77c2522ad118",
+    "url": "/sf-hq/static/media/roboto-medium-webfont.35658d0d.woff"
+  },
+  {
+    "revision": "366cff898a10d9eaa26c3d07c13ebcbd",
+    "url": "/sf-hq/static/media/roboto-medium-webfont.366cff89.woff2"
+  },
+  {
+    "revision": "095f16d4289a38648cdec2be2f031d28",
+    "url": "/sf-hq/static/media/roboto-regular-webfont.095f16d4.woff2"
+  },
+  {
+    "revision": "8e9cbda4cef8580a8e5e620cadf7ffec",
+    "url": "/sf-hq/static/media/roboto-regular-webfont.8e9cbda4.woff"
+  }
+]);
