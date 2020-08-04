b2e6384ba6aef06d4d6ccffd2fbb27bdce2bde90
Nicolo' Fuccella <nico@soluzionifutura.it>
2020-08-04 12:56:11 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import OrganizationGantt from "./components/OrganizationGantt"
 import Wiki from "./components/Wiki"
 import Home from "./components/Home"
 
 const App = () => {
   return (
-    <Router basename='/'>
+    <Router basename={"/"}>
       <Switch>
-        <Route exact path="/" component={ Home } />
+        <Route path="/" component={ Home } />
         <Route path="/gantt/:organization" component={ OrganizationGantt } />
         <Route path="/wiki/:organization" render={ Wiki } />
       </Switch>
